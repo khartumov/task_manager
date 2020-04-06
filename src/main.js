c8e6ee24cb448b109b39eedd0a242e6d4d01e224
khartumov <khartumov.dev@gmail.com>
2020-04-06 17:46:51 +0300

    add less-loader & vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 new Vue({
   store,
