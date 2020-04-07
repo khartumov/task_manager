2a0fb77c0fce8c4234f19bc095f7e7348f49bf39
khartumov <khartumov.dev@gmail.com>
2020-04-07 14:28:43 +0300

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
+import {
+  LayoutPlugin,
+  IconsPlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormDatepickerPlugin,
+  FormSelectPlugin,
+  TooltipPlugin
+} from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(LayoutPlugin)
+Vue.use(IconsPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(FormDatepickerPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(TooltipPlugin)
 
 new Vue({
   store,
