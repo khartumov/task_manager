4d6331223159840fef21d786fe96811175adfa87
khartumov <khartumov.dev@gmail.com>
2020-04-11 11:31:35 +0300

    add filter by status

---
+++ src/components/SelectStatus.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-form-select
+    v-model="selectedStatus"
+    :options="statusList"
+    @input="$emit('selectStatus', selectedStatus)"
+  />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selectedStatus: null,
+      statusList: [
+        {
+          value: null,
+          text: 'Все'
+        },
+        {
+          value: 'progress',
+          text: 'В работе'
+        },
+        {
+          value: 'expired',
+          text: 'Просроченные'
+        },
+        {
+          value: 'completed',
+          text: 'Завершенные'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -7,17 +7,12 @@
           md="6"
           offset-md="1"
         >
-          <Search
-            @searchByTitle="filterByTitle"
-          />
+          <Search @searchByTitle="filterByTitle" />
         </b-col>
         <b-col
           md="4"
         >
-          <b-form-select
-            v-model="selectedStatus"
-            :options="statusList"
-          />
+          <SelectStatus @selectStatus="filterByStatus" />
         </b-col>
       </b-row>
       <b-row>
@@ -25,9 +20,7 @@
           lg="10"
           offset-lg="1"
         >
-          <TaskList
-            :tasks="filteredTasks"
-          />
+          <TaskList :tasks="filteredTasks" />
         </b-col>
       </b-row>
       <router-link
@@ -47,36 +40,19 @@
 <script>
 import TaskList from '@/components/TaskList'
 import Search from '@/components/Search'
+import SelectStatus from '@/components/SelectStatus'
 
 export default {
   name: 'tasks',
 
   components: {
     TaskList,
-    Search
+    Search,
+    SelectStatus
   },
 
   data () {
     return {
-      selectedStatus: null,
-      statusList: [
-        {
-          value: null,
-          text: 'Все'
-        },
-        {
-          value: 'progress',
-          text: 'В работе'
-        },
-        {
-          value: 'expired',
-          text: 'Просроченные'
-        },
-        {
-          value: 'completed',
-          text: 'Завершенные'
-        }
-      ],
       tasks: [
         {
           id: 1,
@@ -101,9 +77,21 @@ export default {
         },
         {
           id: 4,
-          title: 'Четвертая задача',
+          title: 'Выучить JS',
           date: new Date(2020, 8, 23),
           status: 'progress'
+        },
+        {
+          id: 5,
+          title: 'Бинарный поиск',
+          date: new Date(2020, 8, 23),
+          status: 'completed'
+        },
+        {
+          id: 6,
+          title: 'Выучить алгоритмы',
+          date: new Date(2020, 9, 23),
+          status: 'completed'
         }
       ],
       filteredTasks: []
@@ -115,6 +103,14 @@ export default {
       this.filteredTasks = this.tasks.filter(({ title }) => {
         return title.toLowerCase().includes(searchText.toLowerCase())
       })
+    },
+
+    filterByStatus (selectedStatus) {
+      if (selectedStatus) {
+        this.filteredTasks = this.tasks.filter(({ status }) => status === selectedStatus)
+      } else {
+        this.filteredTasks = this.tasks
+      }
     }
   },
 
